5c4e9d82922edfc0edac7a4fb84db9b2957b5d99
Psalmosky <oluwaseyiomotayo1@gmail.com>
2022-09-13 00:24:34 -0700

    prints 10 times the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
