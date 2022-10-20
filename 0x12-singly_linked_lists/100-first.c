98b4040e7c1c3314d19bb092869ee92e46c2711a
Psalmosky <oluwaseyiomotayo1@gmail.com>
2022-10-20 04:29:02 -0700

    function that prints some messages

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/**
- * startupfunc - executes before main function is executed.
- * Return: nothing./
- */
+void __attribute__((constructor)) hare(void);
 
-void startupfunc(void) __attribute__ ((constructor));
-void startupfunc(void)
+/**
+ * hare - Prints a string before the
+ *	main function is executed.
+ */
+void hare(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
